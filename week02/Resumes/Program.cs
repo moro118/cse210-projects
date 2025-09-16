7c799c17f600be8b60c3203285ecbfc95539560f
moro118 <mvillalobosortega@byupathway.edu>
2025-09-16 12:09:50 -0600

    new resume complete assignment

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Tech Corp";
+        job1._startYear = "2020";
+        job1._endYear = "2023";
+      
+        Job job2 = new Job();
+        job2._jobTitle = "Senior Developer";
+        job2._company = "Innovate LLC";
+        job2._startYear = "2023";
+        job2._endYear = "Present";
+  
+
+        Resume myResume = new Resume();
+        myResume._name = "John Doe";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
+
     }
 }
